32e8a79a1b10f3083c777f0333bea873d9538d85
igor tverdokhlebov <tverdokhlebov.igor2013@yandex.ru>
2022-09-05 18:45:38 +0300

    this

--- Work/main.js
+++ Work/main.js
@@ -2,8 +2,26 @@ const developer = {
   name: "Vasiliy",
   surname: "Petrov",
   age: 30,
-  isMariend: true,
-  isMaried: ["JavaScript", "TypeScript", "CSS"],
+  isMaried: false,
+  skills: ["JavaScript", "TypeScript", "CSS"],
+  addAge() {
+    this.age++;
+    console.log(this);
+  },
+  getMarried() {
+    this.isMaried = true;
+  },
 };
 
-console.log(developer.name);
+developer.addAge();
+
+// console.log(developer.skills);
+// for(let prop in developer){
+//   console.log(prop, developer[prop]);
+// }
+
+// const numbers=[1, 2, 3, 4, 5];
+
+// for(let number of numbers){
+//   console.log(number);
+// }
